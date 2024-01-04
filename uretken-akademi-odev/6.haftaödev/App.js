31500212575de4f0b0026ffc7d029c44ad226e6d
gönülevren <121257548+evrenev@users.noreply.github.com>
2024-01-04 15:13:40 +0300

    Add files via upload

---
+++ uretken-akademi-odev/6.haftaödev/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import Sicaklikdönüşüm from "./components/Sicaklikdönüşüm";
+function App() {
+  return (
+    <div>
+      <Sicaklikdönüşüm />
+    </div>
+  );
+}
+
+export default App;
